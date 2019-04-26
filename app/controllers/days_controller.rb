76c4b0d68a89ef81f65d7da1e574e7fa9d699a5c
eankarst <eankarst@uncc.edu>
2019-04-26 03:16:34 +0000

    Added Edit and Destroy functionality

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,8 +1,48 @@
 class DaysController < ApplicationController
     def new
+        @day = Day.new
     end
     
     def create
-        render plain: params[:day].inspect
+        @day = Day.new(day_params)
+        
+        if @day.save
+            redirect_to @day
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+        @day = Day.find(params[:id])
+    end
+    
+    def update
+        @day = Day.find(params[:id])
+        if @day.update(day_params)
+            redirect_to @day
+        else
+            render 'edit'
+        end
+    end
+    
+    def show
+        @day = Day.find(params[:id])
+    end
+    
+    def destroy
+        @day = Day.find(params[:id])
+        @day.destroy
+        
+        redirect_to days_path
+    end
+    
+    def index
+        @days = Day.all
     end
 end
+
+private
+    def day_params
+        params.require(:day).permit(:date)
+    end
