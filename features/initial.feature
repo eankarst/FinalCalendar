250e647bf71d3e13128025de0ab481db69864564
ryo5678 <ryo5678craft@gmail.com>
2019-04-28 16:18:06 +0000

    Fixed first test and removed the 2nd

--- features/initial.feature
+++ features/initial.feature
@@ -1,13 +1,13 @@
-Feature: Sign in
-
+Feature: Get to a day in the calendar
     As a user
     So that I can manage my schedule
-    I want to be able to log in to the webapp
+    I want to be able to get into a calendar
                                                                                               
 Scenario: As a user I want to be able to navigate from the login screen to the schedule screen
- Given I am on the login screen
- When I click on the "Username:" field
- Then I should see the "Eric" field
- When I click on the "Eric" field and I click on the "Submit" link
+ Given I am on the home page
+ When I click on the "Calendar" link
  Then I should be on the "Calendar for the Year 2019" page
- And I should see the "1" field
+ When I click on the "1" link 
+ Then I should be on the "TBD" page 
+ And I should see the "TBD" field
+ And I should see the "TBD" field
--- features/step_definitions/blog_steps.rb
+++ features/step_definitions/blog_steps.rb
@@ -1,34 +1,8 @@
-Given("I am on the {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I am on the home page") do
+  visit root_path
 end
 
-When("I click on the {string} field") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should be on the {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should see the {string} field") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click on the {string} field and I click on the {string} link") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given("I am on the login screen") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-# 2nd Scenario for Edit.feature
-
-Given("I am on the {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click on the {string} field") do |string|
+When("I click on the {string} link") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
@@ -39,8 +13,3 @@ end
 Then("I should see the {string} field") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
-
-When("I click on the {string} field and I click on the {string} link") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
